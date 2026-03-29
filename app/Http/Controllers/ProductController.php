67d93e21ef97aacc5348bd5fcd54f55275f4570d
itschool <itschool@tou.edu.kz>
2026-03-29 12:01:29 +0500

    1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public $title;
-    private $age;
-    protected $name;
-
-    public function get(Request $request) {
-        
+    public function index(Request $request) {
+        //отдаём все продукты
+        echo 'Product Controller: index';
     }
+
+    public function create() {
+        $products = Product::all();
+        dd($products);
+    }
+    
+
+    public function html() {
+        return view('about', [
+            'title' => 'About Page',
+            'text' => 'Доброго полудня!',
+            'is_on' => true
+        ]);
+    }
+
+    // public function create метод get
+    //  public function store метод post
+    //  public function edit  метод put
+    //  public function update
 }
 
-$controller = new ProductController;
-$controller->title; 
-$controller->get();
+// $controller = new ProductController;
+// $controller->title; 
+// $controller->get();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index']);
+
+Route::get('/contacts', [ProductController::class, 'create']);
+
+Route::get('/about', [ProductController::class, 'html']);
+
